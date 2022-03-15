f6fe3ace6fc797df16d7af1504a1875e18bc8fbf
herkulessi <31536990+herkulessi@users.noreply.github.com>
2022-03-15 19:57:49 +0100

    Why do we have 2 different 2/2e and 2/2f? BTW Does git support symlinks?

--- Woche2/Aufgabe2/2e_int2ascii.vhd
+++ Woche2/Aufgabe2/2e_int2ascii.vhd
@@ -22,25 +22,28 @@ BEGIN
         v_bcd1 := "0000";
         v_number := to_unsigned(i_number, v_number'length);
 
+        
+
         FOR i IN 0 TO 7 LOOP
             --TODO: Pseudocode umsetzen
-            if v_bcd0 > 4 then
+            IF v_bcd0 > 4 then
                 v_bcd0 := v_bcd0 + 3;
-            end if;
-            if v_bcd1 > 4 then
+            END IF;
+            IF v_bcd1 > 4 then
                 v_bcd1 := v_bcd1 + 3;
-            end if;
+            END IF;
 
-            v_bcd1 := left_shift(v_bcd1, 1);
+
+            v_bcd1 := shift_left(v_bcd1, 1);
             v_bcd1(0) := v_bcd0(3);
 
-            v_bcd0 := left_shift(v_bcd0, 1);
+            v_bcd0 := shift_left(v_bcd0, 1);
             v_bcd0(0) := v_number(7 - i); -- Laut Pseudocode: 8, aber 7 da A 0-indexed ist
             ----------------------------
         END LOOP;
         s_bcd0 <= v_bcd0;
         s_bcd1 <= v_bcd1;
     END PROCESS;
-    o_ascii0 <= "0011" & s_bcd0; -- converting to 8 bits ascii
-    o_ascii1 <= "0011" & s_bcd1; -- converting to 8 bits ascii
+    o_ascii0 <= "0011" & std_logic_vector(s_bcd0); -- converting to 8 bits ascii
+    o_ascii1 <= "0011" & std_logic_vector(s_bcd1); -- converting to 8 bits ascii
 END ARCHITECTURE behavioral;
