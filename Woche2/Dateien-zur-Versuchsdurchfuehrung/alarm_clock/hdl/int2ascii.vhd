a2ab93311119c2b2340491b2bed9fb99c7e6a8df
herkulessi <31536990+herkulessi@users.noreply.github.com>
2022-03-15 19:46:39 +0100

    VHDL Debugging was painful. Enjoy the fruit of my Labour

--- Woche2/Dateien-zur-Versuchsdurchfuehrung/alarm_clock/hdl/int2ascii.vhd
+++ Woche2/Dateien-zur-Versuchsdurchfuehrung/alarm_clock/hdl/int2ascii.vhd
@@ -34,16 +34,16 @@ BEGIN
             end if;
 
 
-            v_bcd1 := left_shift(v_bcd1, 1);
+            v_bcd1 := shift_left(v_bcd1, 1);
             v_bcd1(0) := v_bcd0(3);
 
-            v_bcd0 := left_shift(v_bcd0, 1);
+            v_bcd0 := shift_left(v_bcd0, 1);
             v_bcd0(0) := v_number(7 - i); -- Laut Pseudocode: 8, aber 7 da A 0-indexed ist
             ----------------------------
         END LOOP;
         s_bcd0 <= v_bcd0;
         s_bcd1 <= v_bcd1;
     END PROCESS;
-    o_ascii0 <= "0011" & s_bcd0; -- converting to 8 bits ascii
-    o_ascii1 <= "0011" & s_bcd1; -- converting to 8 bits ascii
+    o_ascii0 <= "0011" & std_logic_vector(s_bcd0); -- converting to 8 bits ascii
+    o_ascii1 <= "0011" & std_logic_vector(s_bcd1); -- converting to 8 bits ascii
 END ARCHITECTURE behavioral;
